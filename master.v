0aa3da7cc3439ea7edb398152a2fb5901ca9f764
Avronil <basuavronil65@gmail.com>
2026-03-24 15:54:20 +0530

    Update master.v

--- master.v
+++ master.v
@@ -1,57 +1,73 @@
+`timescale 1ns / 1ps
+
 module master(
- input clk, start, miso, rst,
- input [7:0] data_out,
- output reg done, cs_n, mosi, slave_clk,
- output reg [7:0] data_in
- );
- reg [7:0] shift_reg;
- reg [2:0] bit_count;
- reg busy;
- always@(posedge clk or negedge rst)
- begin
-  if (!rst)
+    input clk,
+    input rst,
+    input start,
+    input miso,
+    input [7:0] data_out,
+
+    output reg mosi,
+    output reg cs_n,
+    output reg slave_clk,
+    output reg done,
+    output reg [7:0] data_in
+);
+
+reg [7:0] shift_reg;
+reg [2:0] bit_count;
+reg busy;
+
+always @(posedge clk or negedge rst)
+begin
+    if (!rst)
     begin
-       slave_clk <= 0;
-       done = 1'b0;
-       cs_n = 1'd1;
-       mosi = 1'd0;
-       data_in = 8'd0;
-       shift_reg = 8'd0;
-       bit_count = 3'd7;
-       busy = 1'd0;
+        slave_clk <= 0;
+        cs_n <= 1;
+        mosi <= 0;
+        done <= 0;
+        busy <= 0;
+        bit_count <= 3'd7;
+        shift_reg <= 0;
+        data_in <= 0;
     end
-  else 
-  begin
-    if (start == 1'b1 && busy == 1'd0)
-       begin
-         busy <= 1'd0;
-         cs_n <= 1'd0;
-         shift_reg <= data_out; 
-       end
-    else if (busy == 1'd1)
-        begin 
-          slave_clk <= ~ slave_clk;
-          if (slave_clk == 1'd0)
-          //drive mosi
-            mosi <= shift_reg;
-          else 
+    else
+    begin
+        done <= 0; // default
+
+        if (start && !busy)
+        begin
+            busy <= 1;
+            cs_n <= 0;
+            shift_reg <= data_out;
+            bit_count <= 3'd7;
+            slave_clk <= 0;
+        end
+        
+        else if (busy)
+        begin
+            slave_clk <= ~slave_clk;
+            if (slave_clk == 0)
             begin
-          //drive miso
-            data_in[bit_count] <= miso;
-            if (bit_count == 0)
-              begin
-                 busy = 1'd0;
-                 cs_n <= 1;
-                 done <= 1;
-              end 
-            else 
-              bit_count <= bit_count -1 ;
+                mosi <= shift_reg[bit_count]; //send
             end
-              
-       end
-  end
-   
- end
-  
-    
+            else
+            begin
+                data_in[bit_count] <= miso; //receive
+
+                if (bit_count == 0)
+                begin
+                    busy <= 0;
+                    cs_n <= 1;
+                    done <= 1;
+                end
+                else
+                begin
+                    bit_count <= bit_count - 1;
+                end
+            end
+        end
+    end
+end
+
 endmodule
