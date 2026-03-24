a9a5c1bb84c38a935e373f9ba4a1da0cdde4fe59
Avronil <basuavronil65@gmail.com>
2026-03-24 15:55:01 +0530

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -1,35 +1,43 @@
+`timescale 1ns / 1ps
+
 module top_module(
- input rst, clk, start, 
- input [7:0] data_out,
- input [7:0] m_out, s_out,
- output [7:0] m_in, s_in,
- output done
-    );
- reg slave_clk;
- reg mosi;
- reg miso;
- 
- master dut1(
- .rst(rst),
- .clk(clk),
- .start(start),
- .data_out(m_out),
- .m_out(m_out),
- .slave_clk(slave_clk),
- .mosi(mosi),
- .done(done),
- .cs_n(cs_n),
- .data_in(m_in)
- );
- 
- slave dut2(
- .rst(rst),
- .slave_clk(slave_clk),
- .mosi(mosi),
- .miso(miso),
- .cs_n(cs_n),
- .data_out(s_out),
- .data_in(s_in)
- );
- 
+    input clk,
+    input rst,
+    input start,
+    input [7:0] master_data,
+    input [7:0] slave_data,
+
+    output done,
+    output [7:0] master_rx,
+    output [7:0] slave_rx
+);
+
+
+wire mosi, miso, cs_n, slave_clk;
+
+
+master u_master (
+    .clk(clk),
+    .rst(rst),
+    .start(start),
+    .miso(miso),
+    .data_out(master_data),
+    .mosi(mosi),
+    .slave_clk(slave_clk),
+    .cs_n(cs_n),
+    .done(done),
+    .data_in(master_rx)
+);
+
+
+slave u_slave (
+    .slave_clk(slave_clk),
+    .cs_n(cs_n),
+    .rst(rst),
+    .mosi(mosi),
+    .data_in(slave_data),
+    .miso(miso),
+    .data_out(slave_rx)
+);
+
 endmodule
