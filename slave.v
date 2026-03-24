e629aa691661820e5c3ae2d6c1f8cd8de98c155c
Avronil <basuavronil65@gmail.com>
2026-03-24 15:54:41 +0530

    Update slave.v

--- slave.v
+++ slave.v
@@ -1,46 +1,40 @@
+`timescale 1ns / 1ps
+
 module slave(
- input rst, slave_clk, mosi, cs_n,
- input [7:0] data_out,
- output reg miso,
- output reg [7:0] data_in
- );
- reg [7:0] shift_reg;
- reg [2:0] bit_count;
- always@(negedge cs_n or negedge rst)
- begin
-  if (!rst)
+    input slave_clk,
+    input cs_n,
+    input rst,
+    input mosi,
+    input [7:0] data_in,   // data to send to master
+
+    output reg miso,
+    output reg [7:0] data_out // received from master
+);
+
+reg [2:0] bit_count;
+
+always @(posedge slave_clk or negedge rst)
+begin
+    if (!rst)
     begin
-      miso <= 1'd0;
-      data_in <= 8'd0; 
-      shift_reg <= 8'd0;
-      bit_count <= 3'd0;
+        bit_count <= 3'd7;
+        data_out <= 0;
+        miso <= 0;
     end
-  else 
+    else if (!cs_n)
     begin
-      shift_reg <= data_out;
-      bit_count <=  3'd7;
+        // 🔹 RECEIVE from master (sample MOSI)
+        data_out[bit_count] <= mosi;
+
+        // 🔹 SEND to master (drive MISO)
+        miso <= data_in[bit_count];
+
+        // 🔹 BIT COUNT UPDATE
+        if (bit_count == 0)
+            bit_count <= 3'd7;
+        else
+            bit_count <= bit_count - 1;
     end
- end
- 
- always@(negedge slave_clk)
- begin
-   //mosi
-   if (!cs_n)
-   data_in <= mosi;
- end 
- 
- always@(posedge slave_clk)
- begin
-   //miso
-   if (!cs_n)
-   begin
-   miso <= shift_reg[bit_count];
-      if (bit_count == 0)
-         bit_count <= 3'd7;
-      else 
-         bit_count <= bit_count - 1;
-   end
- end
-   
+end
 
 endmodule
